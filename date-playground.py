0d60c00f965e140e82b76e788d089537db71968a
AaronHand <spottydj@gmail.com>
2016-02-09 14:33:54 -0800

    added a file, changes shit

--- date-playground.py
+++ date-playground.py
@@ -6,5 +6,10 @@ currentdate = datetime.date(2006,1,1)
 enddate = datetime.date(2006,4,19)
 while currentdate <= enddate:
    #print(str(currentdate.year) + '_'+ str(currentdate.month))
-   print(currentdate)
+
+   #formatted 2-digit month
+   print('{:02d}'.format(currentdate.month))
+
+   #formatted 4-digit year
+   print('{:2d}'.format(currentdate.year))
    currentdate += datetime.timedelta(+1)
--- scrape2.py
+++ scrape2.py
@@ -10,26 +10,25 @@ driver = webdriver.Firefox()
 scrapeData = []
 
 
-while startDate <= ENDDATE:
+
+with open('stuff.txt','w') as f:
+
+    while startDate <= ENDDATE:
 
 
 
-    #print(str(currentdate.year) + '_'+ str(currentdate.month))
+        #print(str(currentdate.year) + '_'+ str(currentdate.month))
 
-    #formatted 2-digit month
-    month = '{:02d}'.format(startDate.month)
+        #formatted 2-digit month
+        month = '{:02d}'.format(startDate.month)
 
-    #formatted 4-digit year
-    year = '{:2d}'.format(startDate.year)
-    startDate += datetime.timedelta(+1)
+        #formatted 4-digit year
+        year = '{:2d}'.format(startDate.year)
+        startDate += datetime.timedelta(+1)
 
-    driver.get("http://www.crossfit.com/mt-archive2/" + str(year) + "_" + month +".html")
-    elem = driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[3]/table/tbody/tr/td/div[13]')
-    scrapeData += elem.text
-
-
-for item in scrapeData:
-    print(item)
+        driver.get("http://www.crossfit.com/mt-archive2/" + str(year) + "_" + month +".html")
+        elem = driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[3]/table/tbody/tr/td/div[13]')
+        f.write(elem.text)
 
 
 
