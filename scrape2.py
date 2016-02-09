09d0e59fdc777955dcec01c791c23c7f2ea37033
AaronHand <spottydj@gmail.com>
2016-02-08 16:32:59 -0800

    formatted date

--- scrape2.py
+++ scrape2.py
@@ -1,16 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import datetime
 
 
 
-
-
+ENDDATE = datetime.date.today()
+startDate = datetime.date(2006,1,1)
 driver = webdriver.Firefox()
-driver.get("http://www.crossfit.com/mt-archive2/2012_10.html")
-elem = driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[3]/table/tbody/tr/td/div[13]')
-assert "No results found." not in driver.page_source
+scrapeData = []
 
-print(elem.text)
+
+while startDate <= ENDDATE:
+
+
+
+    #print(str(currentdate.year) + '_'+ str(currentdate.month))
+
+    #formatted 2-digit month
+    month = '{:02d}'.format(startDate.month)
+
+    #formatted 4-digit year
+    year = '{:2d}'.format(startDate.year)
+    startDate += datetime.timedelta(+1)
+
+    driver.get("http://www.crossfit.com/mt-archive2/" + str(year) + "_" + month +".html")
+    elem = driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[3]/table/tbody/tr/td/div[13]')
+    scrapeData += elem.text
+
+
+for item in scrapeData:
+    print(item)
+
 
 
 driver.close()
